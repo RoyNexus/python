7be620e1cf51fd0daae66c599b553d33c16505e5
RoyNexus <pacorg@gmail.com>
2014-10-10 18:59:16 +0200

    Homework 2 updated with solutions

--- homework2.py
+++ homework2.py
@@ -41,7 +41,7 @@ nan = no information about any event.
 
 def find_events(ls_symbols, d_data):
     ''' Finding the event dataframe '''
-    df_close = d_data['close']
+    df_close = d_data['actual_close']
     ts_market = df_close['SPY']
 
     print "Finding Events"
@@ -58,15 +58,17 @@ def find_events(ls_symbols, d_data):
             # Calculating the returns for this timestamp
             f_symprice_today = df_close[s_sym].ix[ldt_timestamps[i]]
             f_symprice_yest = df_close[s_sym].ix[ldt_timestamps[i - 1]]
-            f_marketprice_today = ts_market.ix[ldt_timestamps[i]]
-            f_marketprice_yest = ts_market.ix[ldt_timestamps[i - 1]]
-            f_symreturn_today = (f_symprice_today / f_symprice_yest) - 1
-            f_marketreturn_today = (f_marketprice_today / f_marketprice_yest) - 1
+            #f_marketprice_today = ts_market.ix[ldt_timestamps[i]]
+            #f_marketprice_yest = ts_market.ix[ldt_timestamps[i - 1]]
+            #f_symreturn_today = (f_symprice_today / f_symprice_yest) - 1
+            #f_marketreturn_today = (f_marketprice_today / f_marketprice_yest) - 1
 
             # Event is found if the symbol is down more then 3% while the
             # market is up more then 2%
-            if f_symreturn_today <= -0.03 and f_marketreturn_today >= 0.02:
-                df_events[s_sym].ix[ldt_timestamps[i]] = 1
+            #if f_symreturn_today <= -0.03 and f_marketreturn_today >= 0.02:
+            #    df_events[s_sym].ix[ldt_timestamps[i]] = 1
+            if f_symprice_yest >= 8.0 and f_symprice_today < 8.0:
+                df_events[s_sym].ix[ldt_timestamps[i]] = 1            
 
     return df_events
 
@@ -77,7 +79,7 @@ if __name__ == '__main__':
     ldt_timestamps = du.getNYSEdays(dt_start, dt_end, dt.timedelta(hours=16))
 
     dataobj = da.DataAccess('Yahoo')
-    ls_symbols = dataobj.get_symbols_from_list('sp5002012')
+    ls_symbols = dataobj.get_symbols_from_list('sp5002008')
     ls_symbols.append('SPY')
 
     ls_keys = ['open', 'high', 'low', 'close', 'volume', 'actual_close']
@@ -92,5 +94,5 @@ if __name__ == '__main__':
     df_events = find_events(ls_symbols, d_data)
     print "Creating Study"
     ep.eventprofiler(df_events, d_data, i_lookback=20, i_lookforward=20,
-                s_filename='MyEventStudy.pdf', b_market_neutral=True, b_errorbars=True,
+                s_filename='MyEventStudy2008_8.pdf', b_market_neutral=True, b_errorbars=True,
                 s_market_sym='SPY')
