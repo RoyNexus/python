0b902f3dee9fc2a4ae3c3d69d9e0b56be210b3d7
= <=>
2014-10-29 17:19:44 +0100

    Homework 5 initial commit

---
+++ bollinger.py
@@ -0,0 +1,18 @@
+# QSTK Imports
+import QSTK.qstkutil.tsutil as tsu
+
+# Third Party Imports
+import numpy as np
+import math as math
+
+class Bollinger:
+
+    def __init__(self, prices):
+        self.prices = prices
+                         
+    def get_value(date):
+        result = 0
+        # Bollinger_val = (price - rolling_mean) / (rolling_std)
+        
+        return result
+                    
---
+++ homework5.py
@@ -0,0 +1,38 @@
+# Bollinger bands simulator
+
+# QSTK Imports
+import QSTK.qstkutil.qsdateutil as du
+import QSTK.qstkutil.tsutil as tsu
+import QSTK.qstkutil.DataAccess as da
+
+# Third Party Imports
+import datetime as dt
+import numpy as np
+import math as math
+
+from bollinger import Bollinger
+
+def get_close_prices (start_date, end_date, symbols):
+    dt_timeofday = dt.timedelta(hours=16)
+    ldt_timestamps = du.getNYSEdays(start_date, end_date, dt_timeofday)    
+    c_dataobj = da.DataAccess('Yahoo', cachestalltime=0)    
+    ls_keys = ['close']
+    ldf_data = c_dataobj.get_data(ldt_timestamps, symbols, ls_keys)
+    d_data = dict(zip(ls_keys, ldf_data))        
+    return d_data['close']    
+    
+
+if __name__ == '__main__':
+    # Inputs
+    start_date = dt.datetime(2009, 1, 1)
+    end_date = dt.datetime(2011, 12, 31)
+    symbols = ['AAPL', 'MSFT', 'IBM']
+    
+    prices = get_close_prices(start_date, end_date, symbols)
+
+    bollinger_obj = Bollinger(price_series)
+    
+                                                
+    print 'Bollinger value: ' + str(bollinger_obj.get_value(current_date))
+    
+
